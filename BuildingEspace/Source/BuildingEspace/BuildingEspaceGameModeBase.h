5ad038349f9d045bcc9d7a8cab988443a3ace92a
John Betts <john.betts@globant.com>
2019-04-20 21:26:15 -0500

    Loading files for the first time

---
+++ BuildingEspace/Source/BuildingEspace/BuildingEspaceGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEspaceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESPACE_API ABuildingEspaceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
